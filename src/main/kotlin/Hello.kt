e97fea0b34acaf9ca8b0c4e6552b14b95e8df1c6
RPL-20 <nurulhakiki689@gmail.com>
2022-03-10 14:46:30 +0700

    Hello pertemuan saya

---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di Kotlin")
+}
+
+// Fun main merupakan fungsi yang digunakan unutk memulai program yang akan dijalankan.
