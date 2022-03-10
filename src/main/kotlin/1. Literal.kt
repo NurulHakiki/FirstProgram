13d91daee711d88b86a2918b2bd1ead05f009573
RPL-20 <nurulhakiki689@gmail.com>
2022-03-10 15:30:19 +0700

    Penambahan Kode Program Literal pada kotlin

---
+++ src/main/kotlin/1. Literal.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var intLiteral = 5 //Program literal menggunakan tipe data Integer
+    var doubleLiteral = .02 //Program literal dengan menggunakan tipe data Double
+    var stringLiteral = "Hello" //Program literal dengan menggunakan tipe data String
+    var charLiteral = '1' //Program literal dengan menggunakan tipe data Character
+    var boolLiteral = true //Program literal dengan menggunakan tipe data Boolean
+    println("intliteral = "+ intLiteral) // Mencetak variabel berdasarkan tipe data
+    println("doubleLiteral = " + doubleLiteral) // Mencetak variabel berdasarkan tipe data
+    println("stringLiteral = " + stringLiteral) // Mencetak variabel berdasarkan tipe data
+    println("charLiteral = " + charLiteral) // Mencetak variabel berdasarkan tipe data
+    println("boolLiteral = " + boolLiteral) // Mencetak variabel berdasarkan tipe data
+}
