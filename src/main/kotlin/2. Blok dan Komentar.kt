3c884dd2013347bc3c561f751e2105a84c9188c6
RPL-20 <nurulhakiki689@gmail.com>
2022-03-10 15:32:59 +0700

    Penambahan Kode Program Blok dan Komentar pada kotlin

---
+++ src/main/kotlin/2. Blok dan Komentar.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di Kotlin")
+
+    //Contoh komentar satu baris
+    /*
+    Contoh Komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
