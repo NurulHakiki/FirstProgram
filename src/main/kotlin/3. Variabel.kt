8a3cc958d005efca8bb7c2671c3b38123d80dc80
RPL-20 <nurulhakiki689@gmail.com>
2022-03-10 15:33:24 +0700

    Penambahan Kode Program Variabel pada kotlin

---
+++ src/main/kotlin/3. Variabel.kt
@@ -0,0 +1,19 @@
+// variabel merupakan tempat yang digunakan untuk menyimpan data yang dapat diubah.
+// dibawah ini terdapat 2 jenis type data system yakni
+
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+    /*
+    type sistem yang dinamis tidak memerlukan adanya deklarasi tipr data pada sebuah variabel,
+    compiler secara otomatis mengenali tipe data dari sebuah variabel
+     */
+
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+
+    /*
+    type data system static mengharuskan kita untuk mendeklarasikan jenis tipe data tertentu
+     */
+}
